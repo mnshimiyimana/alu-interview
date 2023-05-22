53324d863c12957cd9f320896852486495317f4d
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-05-22 21:44:55 +0200

    Operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0"""
+
+
+def minOperations(n):
+    """Function that calculates the fewest number of operations needed to
+    -result in exactly n H characters in the file"""
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return 0
+
+
+if __name__ == "__main__":
+    print(minOperations(19170307))
+    print(minOperations(972))
+    print(minOperations(2147483640))
